940af157a2effdff6cc0a4776b078c19531a8252
dataflo <florian.krempl@outlook.com>
2024-02-11 19:33:51 +0100

    fixed document typing

--- mtg/objects/document.py
+++ mtg/objects/document.py
@@ -1,11 +1,14 @@
 from pydantic import BaseModel, Field
+from typing import Union
 
 
 class Document(BaseModel):
     name: str  # text for display
     text: str  # text for vectorizing
     url: str  # link to source
-    metadata: dict[str, str] = Field(default_factory=dict)  # more info
+    metadata: dict[str, Union[str, list[str]]] = Field(
+        default_factory=dict
+    )  # more info
     keywords: list[str] = Field(default_factory=list)  # keywords for document
 
     def __repr__(self):
